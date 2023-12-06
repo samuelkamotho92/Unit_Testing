7c54023704d1ba31b84078bc13a01a926fcf49de
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-12-06 21:33:10 +0300

    worked on subarray

---
+++ ChunkArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssignments
+{
+    public class ChunkArray
+    {
+
+        public List<string>  getSubArray( int[] arrays, int chunk)
+        {
+            int[] arrayVal = arrays;
+            int chunkSize = chunk;
+
+            //create a list to store result
+
+            List<List<int>> results = new List<List<int>>();
+            List<string> newArrayVal = new List<string>();
+            for (int i = 0; i<arrayVal.Length; i+=chunkSize)
+            {
+                var chunkElement = arrayVal.Skip(i).Take(chunkSize).ToList();
+                results.Add(chunkElement);
+            }
+            foreach (var subArray in results)
+            {
+                newArrayVal.Add($"[{string.Join(", ", subArray)}]");
+            }
+            return newArrayVal;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,3 +4,16 @@ using TestAssignments;
 Console.WriteLine("Change the world");
 CapitalizeWords capitalizeWords = new CapitalizeWords();
 capitalizeWords.capitalizeWords("Best day of my life");
+
+ChunkArray chunkArray = new ChunkArray();
+int[] arrayVal = { 1, 2, 3, 3, 4, 5, 6, 7 };
+int chunk = 3;
+chunkArray.getSubArray(arrayVal,chunk);
+string [] values = { "[[ 1, 2, 3 ], [ 3, 4, 5 ], [ 6, 7 ] ]", "[[ 1 ], [ 2 ], [ 3 ], [ 5 ], [ 6 ], [ 7 ] ]" };
+foreach (var item in values)
+{
+/*    Console.WriteLine(item);*/
+}
+List<string> myStrOut = new List<string>(values);
+
+/*Console.WriteLine(myStrOut[1]);*/
